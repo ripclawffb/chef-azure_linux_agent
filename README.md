1260ee5e7c1ae507aedeb331c0b84f02c7a064dc
Asif Shaikh <ripclaw_ffb@hotmail.com>
2020-05-21 18:33:40 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# azure_pipelines_agent_linux
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: azure_pipelines_agent_linux
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
